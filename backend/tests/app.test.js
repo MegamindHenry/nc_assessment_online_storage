758978def78dba9f6ff1d2ad17326505d42041a1
Xuefeng Luo <hakunamatatahenry@gmail.com>
2022-11-02 12:42:30 +0100

    remove one test function

--- backend/tests/app.test.js
+++ backend/tests/app.test.js
@@ -24,14 +24,14 @@ describe('POST', () => {
       .expect({ hello1: 'hello1' });
   });
 
-  // 200 status code
-  // json
-  // { hello1: 'hello1' }
-  it('test /', async () => {
-    await request(app)
-      .post('/')
-      .send({ hello11: 'hello11' })
-      .expect(200)
-      .expect({ hello11: 'hello11' });
-  });
+  // // 200 status code
+  // // json
+  // // { hello1: 'hello1' }
+  // it('test /', async () => {
+  //   await request(app)
+  //     .post('/')
+  //     .send({ hello11: 'hello11' })
+  //     .expect(200)
+  //     .expect({ hello11: 'hello11' });
+  // });
 });
